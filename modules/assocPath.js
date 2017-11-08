0749657a66e4f372faaaf0584c5dfd0ba99c3421
Dejan Toteff <deyan8284@gmail.com>
2017-11-09 00:10:07 +0200

    start R.assocPath remove isObject

--- modules/assocPath.js
+++ modules/assocPath.js
@@ -1,3 +1,9 @@
-export default function assocPath(x){
+export default function assocPath(path, x, obj){
+  if(arguments.length === 2){
+    return objHolder => assocPath(path, x, objHolder)
+  }else if(arguments.length === 1){
+    return (xHolder,objHolder => assocPath(path, x, objHolder)
+
+  }
   return x
 }
