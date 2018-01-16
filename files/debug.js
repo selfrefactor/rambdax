d4919d698e925cc1ae317a0cb361724d81b2eb33
Dejan Toteff <deyan8284@gmail.com>
2018-01-16 18:37:56 +0200

    chore@progress: readme with repl links

--- files/debug.js
+++ files/debug.js
@@ -1,9 +1,46 @@
-const R = require('rambda')
+const {
+  match,
+  init,
+  replace,
+  dropLast,
+} = require('rambda')
 const {readFileSync} = require('fs')
 const {resolve} = require('path')
 const {rambdaREPL} = require('rambda-repl')
 
+const MARKER_SOURCE = '[Source]'
+const MARKER_METHOD = '####'
+
+function getMethod(sourceLink){
+  const baseURL = '(https://github.com/selfrefactor/rambda/tree/master/modules/'
+  const fileName = replace(
+    baseURL,
+    '',
+    sourceLink
+  )
+  
+  return dropLast(4, fileName.trim())
+}
+
+function getContentWithREPL(input){
+  const [,sourceLink] = input.split(MARKER_SOURCE)
+  const method = getMethod(sourceLink)
+  let a
+}
+
 void function name() {
-  let a = rambdaREPL('const result = 2')
+  const filePath = resolve(
+    __dirname,
+    '../README.md'
+  )
+  const content = readFileSync(filePath).toString()
+
+  const contentWithREPL = content.split(MARKER_METHOD).map(singleMethod => {
+    if(singleMethod.includes(MARKER_SOURCE)){
+      return getContentWithREPL(singleMethod)
+    }else{
+      return singleMethod
+    }
+  })
   let aa
 }()
