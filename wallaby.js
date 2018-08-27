2587cc7b4930586d2674ff9ef8e80e75d87eec38
Dejan Toteff <deyan8284@gmail.com>
2018-08-27 15:17:24 +0300

    chore: wallaby init

---
+++ wallaby.js
@@ -0,0 +1,31 @@
+module.exports = function (wallaby){
+  return {
+    files: [
+      'rambdax.js',
+      'package.json',
+      'modules/**/*.js'
+    ],
+    tests: [
+      '__tests__/**/*.js'
+    ],
+    env: {
+      type: 'node',
+      kind:'chrome',
+      runner: 'node'
+    },
+    projectCacheDir: 'node_modules',
+    compilers: {
+      '**/*.js?(x)': wallaby.compilers.babel()
+    },
+    workers: {
+      reload: true,
+      initial: 4,
+      regular: 2
+    },
+    delays: {
+      run: 1000
+    },
+    testFramework: 'jest',
+    debug:true
+  }
+}
