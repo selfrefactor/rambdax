d1b873062b6903c3c96ea00a1e30462badece33f
Dejan Toteff <deyan8284@gmail.com>
2018-09-26 15:35:20 +0300

    feat@method: change

---
+++ modules/change.js
@@ -0,0 +1,56 @@
+// Its lodash's set method taken from 
+// https://github.com/lodash/lodash/blob/4.5.0-npm-packages
+import set from './_internals/set'
+
+const isObject = (x) => {
+  const ok = x !== null && !Array.isArray(x) && typeof x === 'object'
+  if (!ok) {
+    return false
+  }
+
+  return Object.keys(x).length > 0
+}
+
+export const change = (origin, pathRaw, rules) => {
+  const willReturn = JSON.parse(JSON.stringify(origin))
+  
+  if (!isObject(rules)) {
+    set(willReturn, pathRaw, rules)
+
+    return willReturn
+  }
+  const path = pathRaw === '' ? '' : `${pathRaw}.`
+
+  for (const ruleKey of Object.keys(rules)) {
+    const rule = rules[ruleKey]
+    if (!isObject(rule)) {
+      set(
+        willReturn,
+        `${path}${ruleKey}`,
+        rule
+      )
+      continue
+    }
+    Object.keys(rule).filter(subruleKey => !isObject(rule[subruleKey])).map((subruleKey) => {
+      const subrule = rule[subruleKey]
+      set(
+        willReturn,
+        `${path}${ruleKey}.${subruleKey}`,
+        subrule
+      )
+    })
+    Object.keys(rule).filter(subruleKey => isObject(rule[subruleKey])).map((subruleKey) => {
+      const subrule = rule[subruleKey]
+      Object.keys(subrule).map((deepKey) => {
+        const deep = rule[subruleKey][deepKey]
+        set(
+          willReturn,
+          `${path}${ruleKey}.${subruleKey}.${deepKey}`,
+          deep
+        )
+      })
+    })
+  }
+
+  return willReturn
+}
---
+++ modules/change.spec.js
@@ -0,0 +1,64 @@
+import { change } from './change'
+
+const origin = {
+  a: 0,
+  foo: {
+    bar: 1,
+    baz: false,
+    bax: {
+      nested: 2
+    }
+  },
+  first: {
+    second: {
+      third: {
+        fourth: 3,
+        fourthA: 4
+      }
+    }
+  }
+}
+
+test('when rule is not an object', () => {
+  const result = change(
+    origin,
+    'foo.bar.baz',
+    7
+  )
+
+  expect(result.foo.bar.baz).toBe(7)
+  expect(result.foo.bax.nested).toBe(2)
+  expect(result.a).toBe(0)
+  expect(result.first.second.third.fourth).toBe(3)
+})
+
+test('preserve origin object if nesting level is below 3', () => {
+  const changeData = {
+    foo: {
+      bar: 7,
+      bax: {
+        bay: 8
+      }
+    },
+    first: {
+      second: {
+        third: {
+          fourth: 9
+        }
+      }
+    }
+  }
+  const result = change(
+    origin,
+    '',
+    changeData
+  )
+
+  expect(result.a).toBe(0)
+  expect(result.foo.bar).toBe(7)
+  expect(result.foo.baz).toBe(false)
+  expect(result.foo.bax.nested).toBe(2)
+  expect(result.foo.bax.bay).toBe(8)
+  expect(result.first.second.third.fourth).toBe(9)
+  expect(result.first.second.third.fourthA).toBe(undefined)
+})
