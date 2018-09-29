63435d6bf6df8dfb389b889512c698a3da928d25
Dejan Toteff <deyan8284@gmail.com>
2018-09-29 23:51:47 +0300

    chore: benchmark jest cli options

--- files/debug.js
+++ files/debug.js
@@ -1,115 +1,38 @@
-const { type, toLower, contains, test, any, all } = require('rambda')
+const testFile = `isValid.spec.js`
 
-function isValid ({ input, schema }) {
-  if (type(input) === 'Object' && type(schema) === 'Object') {
+const command0 = `node node_modules/jest/bin/jest.js -- modules/${testFile}`
+const command1 = `node node_modules/jest/bin/jest.js -- modules/${testFile} --runInBand`
+const command2 = `node node_modules/jest/bin/jest.js -- modules/${testFile} --runInBand --env=node`
+const command3 = `node node_modules/jest/bin/jest.js -- modules/${testFile} --env=node`
 
-    let flag = true
-    const boom = (boomFlag) => {
-      if(!boomFlag){
-        flag = false
-      }
-    }
+const commands = [
+  command1,
+  command2,
+  command0,
+  command3,
+]
 
-    for (const requirement in schema) {
-    
-      if (flag) {
-        const rule = schema[ requirement ]
-        const ruleType = type(rule)
-        const inputProp = input[ requirement ]
-        const inputPropType = type(input[ requirement ])
+const R = require('../dist/rambdax.cjs')
+const {exec} = require('child_process')
+const cwd = process.cwd()
 
-        if (
-          ruleType === 'Object'
-        ) {
-          // This rule is standalone schema - schema = {a: {b: 'string'}}  
-          const isValidResult = isValid({
-            input: inputProp,
-            schema: rule
-          })
-          boom(isValidResult)
-
-        } else if (
-          ruleType === 'String'
-        ) {
-          // rule is concrete rule such as 'number' so two types are compared
-          boom(toLower(inputPropType) === rule) 
-
-        } else if (
-          typeof rule === 'function'
-        ) {
-          // rule is function so we pass to it the input
-          boom(rule(inputProp))
-
-        } else if (
-          ruleType === 'Array' &&
-          inputPropType === 'String'
-        ) {
-          // enum case | rule is like a: ['foo', 'bar']
-          boom(contains(inputProp, rule))
-
-        } else if (
-          ruleType === 'Array' &&
-          rule.length === 1 &&  
-          inputPropType === 'Array'
-        ) {
-          // 1. array of type | rule is like a: ['number']
-          // 2. rule is like a: [{from: 'string'}]
-          
-          const currentRule = rule[0]
-          const currentRuleType = type(rule[0])
-          // Check if rule is invalid
-          boom(currentRuleType === 'String' || currentRuleType === 'Object')
-          
-          if(currentRuleType === 'String'){
-
-            // 1. array of type
-            const isInvalidResult = any(
-              inputPropInstance => type(inputPropInstance).toLowerCase() !== currentRule,
-              inputProp
-            )
-            boom(!isInvalidResult)
-          }
-          
-          if(currentRuleType === 'Object'){
-
-            // 2. rule is like a: [{from: 'string'}]
-            const isValidResult = all(
-              inputPropInstance => isValid({input: inputPropInstance, schema: currentRule}),
-              inputProp
-            )
-            boom(isValidResult)
-          }
-
-        } else if (
-          ruleType === 'RegExp' &&
-          inputPropType === 'String'
-        ) {
-
-          boom(test(rule, inputProp))
-
-        } else {
-
-          boom(false)
-
-        }
-      }
-    }
-
-    return flag
+void async function(){
+  let counter = -1
+  for(const command of commands){
+    counter++
+    await execCommand(command, counter)
   }
+}()
 
-  return false
+function execCommand(command, tag){
+  console.time(tag)
+	return new Promise((resolve, reject) => {
+    exec(command, {cwd}, (error, stdout, stderr) => {
+      console.timeEnd(tag)
+			if (error) {
+				return reject({ error, stdout, stderr });
+			}
+			resolve({ stdout, stderr });
+		});
+	});
 }
-
-const input = {
-  a: [{
-    from: 'more'
-  }]
-}
-
-const schema = {
-  a: [{from: 'string'}]
-}
-
-const result = isValid({input, schema})
-console.log(result)
