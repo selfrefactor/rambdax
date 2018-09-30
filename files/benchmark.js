42318290ff4fdae5902f12dd5551bb1e52505d8d
Dejan Toteff <deyan8284@gmail.com>
2018-09-30 03:33:31 +0300

    chore: benchmark memoize versions

--- files/benchmark.js
+++ files/benchmark.js
@@ -1,3 +1,4 @@
+const assert = require('assert')
 const Benchmark = require('benchmark')
 const benchmarks = require('beautify-benchmark')
 const R = require('../dist/rambdax.cjs')
@@ -5,14 +6,90 @@ const R = require('../dist/rambdax.cjs')
 const suite = new Benchmark.Suite()
 
 const suiteToRun = suite
+  .add('plain', () => {
+    let counter = 0
+    const fn = (a, b) => {
+      counter++
+      return a + b
+    }  
+
+    const memoized = fn
+    const expected = 5
+    
+    memoized(1, 2)
+    memoized(1, 2)
+    memoized(1, 2)
+    memoized(2, 2)
+    memoized(1, 2)
+    assert.ok(counter <= expected)
+  })
   .add('current', () => {
-    const a = 1
+    let counter = 0
+    const fn = (a, b) => {
+      counter++
+      return a + b
+    }  
+
+    const memoized = R.memoize(fn)
+    const expected = 2
+    
+    memoized(1, 2)
+    memoized(1, 2)
+    memoized(1, 2)
+    memoized(2, 2)
+    memoized(1, 2)
+    assert.ok(counter <= expected)
   })
   .add('next', () => {
-    const a = 1
+    let counter = 0
+    const fn = (a, b) => {
+      counter++
+      return a + b
+    }  
+
+    const memoized = memoizeNext(fn)
+    const expected = 2
+    
+    memoized(1, 2)
+    memoized(1, 2)
+    memoized(1, 2)
+    memoized(2, 2)
+    memoized(1, 2)
+    assert.ok(counter <= expected)
   })
 
 suiteToRun
   .on('cycle', ({target}) => benchmarks.add(target))  
   .on('complete', () => benchmarks.log()) 
   .run()
+
+function memoizeNext(fn){
+  const cache = new Map()
+  return function(inputRaw){
+    const input = inputRaw.toString()
+    if(!cache.has(input)){
+      const result = fn(inputRaw)
+      
+      cache.set(input, result)
+    }
+    return cache.get(input)
+  }
+}
+
+function base(memoizeFn){
+  let counter = 0
+  const fn = (a, b) => {
+    counter++
+    return a + b
+  }  
+
+  const memoized = memoizeFn ? R.memoize(fn) : fn
+  const expected = memoizeFn ? 2 : 5
+  
+  memoized(1, 2)
+  memoized(1, 2)
+  memoized(1, 2)
+  memoized(2, 2)
+  memoized(1, 2)
+  assert.ok(counter <= expected)
+}
--- modules/memoize.spec.js
+++ modules/memoize.spec.js
@@ -1,99 +1,95 @@
-const R = require('../rambdax')
+import memoize from './memoize'
 
-describe('memoize', () => {
-  it('normal function', () => {
-    let counter = 0
+test('', () => {
+  let counter = 0
 
-    const fn = ({ a, b, c }) => {
-      counter++
+  const fn = ({ a, b, c }) => {
+    counter++
 
-      return a + b - c
-    }
-    const memoized = R.memoize(fn)
-    expect(memoized({
-      a : 1,
-      c : 3,
-      b : 2,
-    })).toBe(0)
-    expect(counter).toBe(1)
-    expect(memoized({
-      b : 2,
-      c : 3,
-      a : 1,
-    })).toBe(0)
-    expect(counter).toBe(1)
-  })
+    return a + b - c
+  }
+  const memoized = memoize(fn)
+  expect(memoized({
+    a : 1,
+    c : 3,
+    b : 2,
+  })).toBe(0)
+  expect(counter).toBe(1)
+  expect(memoized({
+    b : 2,
+    c : 3,
+    a : 1,
+  })).toBe(0)
+  expect(counter).toBe(1)
 })
 
-describe('memoize', () => {
-  it('normal function', () => {
-    let counter = 0
-    const fn = (a, b) => {
-      counter++
+test('normal function', () => {
+  let counter = 0
+  const fn = (a, b) => {
+    counter++
 
-      return a + b
-    }
-    const memoized = R.memoize(fn)
-    expect(memoized(1, 2)).toBe(3)
-    expect(memoized(1, 2)).toBe(3)
-    expect(memoized(1, 2)).toBe(3)
-    expect(counter).toBe(1)
-    expect(memoized(2, 2)).toBe(4)
-    expect(counter).toBe(2)
-    expect(memoized(1, 2)).toBe(3)
-    expect(counter).toBe(2)
+    return a + b
+  }
+  const memoized = memoize(fn)
+  expect(memoized(1, 2)).toBe(3)
+  expect(memoized(1, 2)).toBe(3)
+  expect(memoized(1, 2)).toBe(3)
+  expect(counter).toBe(1)
+  expect(memoized(2, 2)).toBe(4)
+  expect(counter).toBe(2)
+  expect(memoized(1, 2)).toBe(3)
+  expect(counter).toBe(2)
+})
+
+test('async function', async () => {
+  let counter = 0
+  const delay = ms => new Promise(resolve => {
+    setTimeout(resolve, ms)
   })
+  const fn = async (ms, a, b) => {
+    await delay(ms)
+    counter++
 
-  it('async function', async () => {
-    let counter = 0
-    const delay = ms => new Promise(resolve => {
-      setTimeout(resolve, ms)
-    })
-    const fn = async (ms, a, b) => {
-      await delay(ms)
-      counter++
+    return a + b
+  }
 
-      return a + b
-    }
+  const memoized = memoize(fn)
+  expect(await memoized(100, 1, 2)).toBe(3)
+  expect(await memoized(100, 1, 2)).toBe(3)
+  expect(await memoized(100, 1, 2)).toBe(3)
+  expect(counter).toBe(1)
+  expect(await memoized(100, 2, 2)).toBe(4)
+  expect(counter).toBe(2)
+  expect(await memoized(100, 1, 2)).toBe(3)
+  expect(counter).toBe(2)
+})
 
-    const memoized = R.memoize(fn)
-    expect(await memoized(100, 1, 2)).toBe(3)
-    expect(await memoized(100, 1, 2)).toBe(3)
-    expect(await memoized(100, 1, 2)).toBe(3)
-    expect(counter).toBe(1)
-    expect(await memoized(100, 2, 2)).toBe(4)
-    expect(counter).toBe(2)
-    expect(await memoized(100, 1, 2)).toBe(3)
-    expect(counter).toBe(2)
+test('s', () => {
+  let count = 0
+  const tester = memoize(n => {
+    count++
+
+    return n + 6
   })
+  tester(5)
+  tester(5)
+  tester(5)
 
-  it('', () => {
-    let count = 0
-    const tester = R.memoize(n => {
-      count++
-
-      return n + 6
-    })
+  expect(
     tester(5)
-    tester(5)
-    tester(5)
+  ).toEqual(11)
 
-    expect(
-      tester(5)
-    ).toEqual(11)
+  expect(
+    count
+  ).toEqual(1)
 
-    expect(
-      count
-    ).toEqual(1)
+  tester(6)
 
+  expect(
     tester(6)
+  ).toEqual(12)
 
-    expect(
-      tester(6)
-    ).toEqual(12)
-
-    expect(
-      count
-    ).toEqual(2)
-  })
+  expect(
+    count
+  ).toEqual(2)
 })
