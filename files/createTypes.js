de4c003f105bd911a100e7e07d18a5385bc0c03f
Dejan Toteff <deyan8284@gmail.com>
2018-10-13 13:39:16 +0300

    chore: fix generate.rambda.exports

--- files/createTypes.js
+++ files/createTypes.js
@@ -1,7 +1,7 @@
-import { readFileSync, writeFileSync } from 'fs-extra'
-import { resolve } from 'path'
-import { inject } from '../rambdax'
-import { between } from 'string-fn'
+const { between } = require('string-fn')
+const { inject } = require('../dist/rambdax')
+const { readFileSync, writeFileSync } = require('fs')
+const { resolve } = require('path')
 
 const PATH = resolve(
   __dirname,
@@ -9,26 +9,27 @@ const PATH = resolve(
 )
 const OUTPUT_PATH = resolve(
   __dirname,
-  '../index.d.ts'
+  '../indexB.d.ts'
 )
 const SOURCE_PATH = resolve(
   __dirname,
   '../files/types.d.ts'
 )
-const XPATH = '/home/s/repos/rambdax/node_modules/rambda/index.d.ts'
-const OUTPUT_XPATH = '/home/s/repos/rambdax/index.d.ts'
-const SOURCE_XPATH = '/home/s/repos/rambdax/files/types.d.ts'
+//is(xPrototype: any, x: any): boolean
+//is(xPrototype: any): (x: any) => boolean
 
-export function createTypes (input) {
+function createTypes() {
   try {
-    const rambda = readFileSync(XPATH).toString()
-    const source = readFileSync(SOURCE_XPATH).toString()
+    const rambda = readFileSync(PATH).toString()
+    const source = readFileSync(SOURCE_PATH).toString()
 
     const found = between(
       rambda,
       '// RAMBDA_START_MARKER',
       '// RAMBDA_END_MARKER',
     )
+    //const foundNoIs = remove
+    //const foundNoIs = removeAll([], found)
     console.log(found)
     const injected = inject(
       found,
@@ -36,8 +37,11 @@ export function createTypes (input) {
       source
     )
 
-    writeFileSync(OUTPUT_XPATH, injected)
+    writeFileSync(OUTPUT_PATH, injected)
+    console.log('done')
   } catch (err) {
     throw err
   }
 }
+
+createTypes()
