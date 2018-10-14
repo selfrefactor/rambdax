2e8b139f14312525db5cea0aa5aff0f68075297c
Dejan Toteff <deyan8284@gmail.com>
2018-10-14 18:43:45 +0300

    chore@dependency: remove

--- files/do.js
+++
@@ -1,8 +0,0 @@
-const { doModule } = require('do')
-const { resolve } = require('path')
-
-doModule({
-  mode         : 'NODE',
-  srcDirectory : resolve(__dirname, '../src'),
-  packageJson  : resolve(__dirname, '../package.json'),
-}).then(console.log)
