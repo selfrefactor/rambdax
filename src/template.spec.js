96dacf20a234b677a349e26475658d7e9ffb7cff
Dejan Toteff <deyan8284@gmail.com>
2018-10-14 22:40:06 +0300

    test@method: template

--- src/template.spec.js
+++ src/template.spec.js
@@ -1,6 +1,21 @@
 import { template } from './template'
 
-test('', () => {
+test('within bracets', () => {
+  const input = 'foo is { {{bar}} } even {{a}} more'
+  const templateInput = {
+    bar : 'BAR',
+    a   : 1,
+  }
+
+  const result = template(input, templateInput)
+  const expectedResult = 'foo is { BAR } even 1 more'
+
+  expect(
+    result
+  ).toEqual(expectedResult)
+})
+
+test('ok', () => {
   const input = 'foo is {{bar}} even {{a}} more'
   const templateInput = {
     bar : 'BAR',
