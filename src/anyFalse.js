b8da8ee4c51416da3b225b53d6e40023ce831d76
Dejan Toteff <deyan8284@gmail.com>
2018-10-16 10:14:50 +0300

    feat@method: any.true any.false

---
+++ src/anyFalse.js
@@ -0,0 +1,12 @@
+export function anyFalse(...inputs){
+
+  let counter = 0
+  while (counter < inputs.length) {
+    if (!inputs[ counter ]) {
+      return true
+    }
+    counter++
+  }
+
+  return false
+}
