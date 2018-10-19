18408275177dc7b13332e9cc3f79dc3be2b2f517
Dejan Toteff <deyan8284@gmail.com>
2018-10-19 23:36:10 +0300

    typings: try.catch is.function

--- files/types.d.ts
+++ files/types.d.ts
@@ -232,6 +232,7 @@ declare namespace R {
     isString(x: any): boolean
     isObject(x: any): boolean
     isPromise(x: any): boolean
+    isFunction(x: any): boolean
 
     mapAsync<T>(fn: Async<any>, x: any[]): Promise<Array<T>>
     mapAsync<T>(fn: AsyncWithProp<any>, x: object): Promise<Array<T>>
@@ -298,6 +299,11 @@ declare namespace R {
     throttle<T>(fn: T, ms: number): ReplaceReturnType<T, void>;    
 
     template(input: string, templateInput: object): string
+
+    tryCatch(
+      fn: Async | Function, 
+      fallback: any
+    ): Promise | Function
     
     where(conditions: object, input: object): boolean
 
