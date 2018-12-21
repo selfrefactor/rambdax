e6c9e83f6cbdbb687e92ebb412c0b4dff4b3cb37
Dejan Toteff <deyan8284@gmail.com>
2018-12-21 19:38:42 +0200

    test@method: merge.deep ramda compatible tests

--- src/mergeDeep.spec.js
+++ src/mergeDeep.spec.js
@@ -17,7 +17,7 @@ const bBase = {
 test('', () =>{
   const result = mergeDeep(aBase,bBase)
   const curryResult = mergeDeep(aBase)(bBase)
-  const expectedResult = {
+  const expected = {
     "age": 40, 
     "name": "evilMe",
     "contact": {
@@ -27,6 +27,31 @@ test('', () =>{
     songs: {title: 'Remains the same'}
   }
 
-  expect(result).toEqual(expectedResult)
-  expect(curryResult).toEqual(expectedResult)
+  expect(result).toEqual(expected)
+  expect(curryResult).toEqual(expected)
+})
+
+test('Ramda compatible test 1', () => {
+  var a = { w: 1, x: 2, y: { z: 3 }};
+  var b = { a: 4, b: 5, c: { d: 6 }};
+  const result = mergeDeep(a, b)
+  const expected = { w: 1, x: 2, y: { z: 3 }, a: 4, b: 5, c: { d: 6 }}
+  
+  expect(result).toEqual(expected)
+})
+
+test('Ramda compatible test 2', () => {
+  var a = { a: { b: 1, c: 2 }, y: 0 };
+  var b = { a: { b: 3, d: 4 }, z: 0 };
+  const result = mergeDeep(a, b)
+  const expected = { a: { b: 3, c: 2, d: 4 }, y: 0, z: 0 }
+  
+  expect(result).toEqual(expected)
+})
+
+test('Ramda compatible test 3', () => {
+  var a = { w: 1, x: { y: 2 }};
+  var result = mergeDeep(a, { x: { y: 3 }});
+  const expected = { w: 1, x: { y: 3 }}
+  expect(result).toEqual(expected)
 })
