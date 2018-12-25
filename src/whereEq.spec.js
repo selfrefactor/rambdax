d96c34e27aaf5d10bd1ee623e04164bc1c24800c
Dejan Toteff <deyan8284@gmail.com>
2018-12-25 14:44:16 +0200

    feat: add r.where.qu method

---
+++ src/whereEq.spec.js
@@ -0,0 +1,27 @@
+import { whereEq } from './whereEq'
+
+test('', () => {
+  const rule = { a : 1 }
+  const input = {
+    a : 1,
+    b : 2,
+  }
+
+  const result = whereEq(rule, input)
+  const expectedResult = true
+
+  expect(result).toEqual(expectedResult)
+})
+
+test('with nested object', () => {
+  const rule = { a : {b: 1} }
+  const input = {
+    a : {b: 1},
+    c : 2,
+  }
+
+  const result = whereEq(rule)(input)
+  const expectedResult = true
+
+  expect(result).toEqual(expectedResult)
+})
