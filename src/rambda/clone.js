76892b0bd4f72daf980161fa90b721c81511f219
Dejan Toteff <deyan8284@gmail.com>
2019-05-20 22:28:55 +0300

    chore@important rambda sync

---
+++ src/rambda/clone.js
@@ -0,0 +1,34 @@
+/**
+ * Creates a deep copy of the value which may contain (nested) `Array`s and
+ * `Object`s, `Number`s, `String`s, `Boolean`s and `Date`s. `Function`s are
+ * assigned by reference rather than copied
+ *
+ * Dispatches to a `clone` method if present.
+ *
+ * @func
+ * @category Object
+ * @sig {*} -> {*}
+ * @param {*} val The object or array to clone
+ * @return {*} A deeply cloned copy of `val`
+ * @example
+ *
+ *      const objects = [{}, {}, {}];
+ *      const objectsClone = R.clone(objects);
+ *      objects === objectsClone; //=> false
+ *      objects[0] === objectsClone[0]; //=> false
+ */
+export function clone (val) {
+  const out = Array.isArray(val) ? Array(val.length) : {}
+
+  for (const key in val) {
+    let v = val[key]
+    out[key] =
+      typeof v === 'object' && v !== null
+        ? v.getTime
+        ? new Date(v.getTime())
+        : clone(v)
+        : v
+  }
+
+  return out
+}
---
+++ src/rambda/isEmpty.js
@@ -0,0 +1,29 @@
+/**
+ * Returns `true` if the given value is its type's empty value; `false`
+ * otherwise.
+ *
+ * @func
+ * @category Logic
+ * @sig a -> Boolean
+ * @param {*} x
+ * @return {Boolean}
+ * @example
+ *
+ *      R.isEmpty([1, 2, 3]);   //=> false
+ *      R.isEmpty([]);          //=> true
+ *      R.isEmpty('');          //=> true
+ *      R.isEmpty(null);        //=> false
+ *      R.isEmpty({});          //=> true
+ *      R.isEmpty({length: 0}); //=> false
+ */
+export function isEmpty (x) {
+  if (Number.isFinite(x) || !x) {
+    return true
+  }
+
+  if (Array.isArray(x)) {
+    return x.length === 0
+  }
+
+  return Object.keys(x).length === 0
+}
