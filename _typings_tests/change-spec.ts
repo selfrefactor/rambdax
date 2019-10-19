cd4301e40f966b0d65c32879bbe95b78cae5ff82
Dejan Toteff <deyan8284@gmail.com>
2019-10-19 08:56:25 +0300

    chore@typescript r.change accept input and output types

--- _typings_tests/change-spec.ts
+++ _typings_tests/change-spec.ts
@@ -1,12 +1,27 @@
 import { change } from 'rambdax'
 
 describe('change', () => {
-  it('number', () => {
+  it('happy', () => {
     const result = change(
       {a: {b: 'foo'}},
       'a.b',
       true
     ); 
     result // $ExpectType object
+  });
+
+  it('with declared types', () => {
+    interface Input {
+      a: {b: string}
+    }
+    interface Output {
+      a: {b: boolean}
+    }
+    const result = change<Input, Output>(
+      {a: {b: 'foo'}},
+      'a.b',
+      true
+    ); 
+    result // $ExpectType Output
   });
 });
