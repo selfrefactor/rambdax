84fddfaefc4e64717ab7a778d5e0f2e56b5cf7de
Dejan Toteff <deyan8284@gmail.com>
2019-10-26 14:51:28 +0300

    feat@stop typings tests | r.run.tests

--- src/extra/runTests.js
+++ src/extra/runTests.js
@@ -1,36 +1,111 @@
 import { omit } from '../rambda/omit'
-import { template } from '../template'
+import { map } from '../rambda/map'
+import { type } from '../rambda/type'
+import { filter } from '../rambda/filter'
+import { maybe } from '../maybe'
 import { headObject } from '../_internals/headObject'
 import { pass as passMethod } from '../pass'
 
-const evaluationsSchema = { label : 'string' }
+const TEST_MODES = [ 'ok', 'fail', 'danger' ]
+const dataPredicate = data => {
+  const filtered = filter(
+    x => {
+      if (type(x) !== 'Object') return true
+      if (Object.keys(x).length === 1) return true
+      if (Object.keys(x).length === 2 && typeof x.label === 'string') return true
+
+      return false
+    },
+    data
+  )
+
+  return Object.keys(filtered).length === Object.keys(data).length
+}
+
+/*
+  TODO
+  R.isValid Array of object passing predicate
+*/
+
+const parseData = map(
+  x => {
+    if (type(x) !== 'Object') return { ok : x }
+
+    if (Object.keys(x).length === 1){
+      const { prop } = headObject(x)
+
+      return TEST_MODES.includes(prop) ?
+        x :
+        { ok : x }
+    }
+    if (Object.keys(x).length === 2 && x.label){
+
+      const { prop } = headObject(omit('label', x))
+
+      return TEST_MODES.includes(prop) ?
+        x :
+        { ok : x }
+    }
+
+    return { ok : x }
+  }
+)
 
 export function runTests(input){
   const pass = passMethod(input)({
-    testSuite   : 'string',
-    evaluations : [ evaluationsSchema ],
+    label : 'string',
+    data  : dataPredicate,
   })
-
   if (describe === undefined || !pass){
     throw new Error('R.runTests.init')
   }
   try {
-    const { testSuite, evaluations, data } = input
+    const { label: suiteLabel, fn, data } = input
+    const counters = {
+      ok     : -1,
+      fail   : -1,
+      danger : -1,
+    }
+    describe(suiteLabel, () => {
+      parseData(data).forEach(dataInstanceRaw => {
+        const withLabel = Object.keys(dataInstanceRaw).length === 2
+        const dataInstance = withLabel ?
+          omit('label', dataInstanceRaw) :
+          dataInstanceRaw
 
-    describe(testSuite, () => {
-      evaluations.forEach(singleEvaluation => {
-        data.forEach(dataInstance => {
-          const { prop: tag, value: x } = headObject(dataInstance)
-          const { value: evaluationFunction } = headObject(
-            omit('label', singleEvaluation)
-          )
+        const { prop: testMode, value: x } = headObject(dataInstance)
 
-          const label = template(singleEvaluation.label, { tag })
+        if (!TEST_MODES.includes(testMode)) return
+        if (!withLabel){
+          counters[ testMode ] = counters[ testMode ] + 1
+        }
 
-          test(label, () => {
-            evaluationFunction(x)
+        const appendLabel = maybe(
+          withLabel,
+          '',
+          counters[ testMode ] > 0 ? ` - ${ counters[ testMode ] }` : '',
+        )
+
+        const testLabel = withLabel ?
+          dataInstanceRaw.label :
+          `${ testMode }${ appendLabel }`
+        
+        if (testMode === 'ok'){
+          test(testLabel, () => {
+            expect(fn(x)).toBeTruthy()
           })
-        })
+        }
+
+        if (testMode === 'fail'){
+          test(testLabel, () => {
+            expect(fn(x)).toBeFalsy()
+          })
+        }
+        if (testMode === 'danger'){
+          test(testLabel, () => {
+            expect(() => fn(x)).toThrow()
+          })
+        }
       })
     })
   } catch (err){
@@ -38,28 +113,3 @@ export function runTests(input){
     throw new Error('R.runTestsCatch')
   }
 }
-
-export const getPositiveEvaluation = ({ label, fn }) => ({
-  label : `{{tag}} - ${ label }`,
-  fn    : x => {
-    expect(fn(x.foo)).toEqual(x.t)
-  },
-})
-
-export const getNegativeEvaluation = ({ label, fn }) => ({
-  label : `{{tag}} - ${ label }`,
-  fn    : x => {
-    expect(fn(x.foo)).not.toEqual(x.f)
-  },
-})
-
-export const getEvaluations = ({ label, fn }) => [
-  getPositiveEvaluation({
-    label,
-    fn,
-  }),
-  getNegativeEvaluation({
-    label,
-    fn,
-  }),
-]
--- src/extra/runTests.spec.js
+++ src/extra/runTests.spec.js
@@ -1,93 +1,90 @@
 import { omit } from '../rambda/omit'
 import { delay } from '../delay'
-import { runTests, getEvaluations, getPositiveEvaluation, getNegativeEvaluation } from './runTests'
+import { runTests } from './runTests'
 
-const whenTrue = {
-  label    : '{{tag}} - when true',
-  whenTrue : x => {
-    expect(typeof x.foo).toEqual(x.t)
+const testFn = x => typeof x.a === 'number'
+
+const testData = [
+  { a : 3 },
+  {
+    ok    : { a : 1 },
+    label : 'happy',
   },
-}
-const whenFalse = {
-  label     : '{{tag}} - when false',
-  whenFalse : x => {
-    expect(typeof x.foo).not.toEqual(x.f)
-  },
-}
-
-const singleCase = {
-  foo : 1,
-  t   : 'number',
-  f   : 'boolean',
-}
-
-const runTestsInput = {
-  testSuite   : 'foo',
-  data        : [ { singleCase } ],
-  evaluations : [ whenTrue, whenFalse ],
-}
-
-test('undefined throws', () => {
-  expect(() => runTests()).toThrow()
-})
-
-test('missing `testSuite`', () => {
-  expect(() =>
-    runTests(omit('testSuite', runTestsInput))
-  ).toThrow()
-})
-
-runTests(runTestsInput)
-
-const singleCaseAsync = {
-  foo : 1,
-  t   : 'RAMBDAX_DELAY',
-  f   : 2,
-}
-
-const whenTrueAsync = {
-  label    : '{{tag}} hey hey what can I do',
-  whenTrue : async x => {
-    const result = await delay(x.foo)
-
-    expect(result).toBe(x.t)
-  },
-}
-
-const runTestsInputAsync = {
-  testSuite   : 'foo',
-  data        : [ { singleCaseAsync } ],
-  evaluations : [ whenTrueAsync ],
-}
-
-runTests(runTestsInputAsync)
-
-const withGetEvaluations = {
-  testSuite   : 'foo',
-  data        : [ { singleCase } ],
-  evaluations : getEvaluations({
-    label : 'BAR',
-    fn    : x => typeof x,
-  }),
-}
-
-runTests(withGetEvaluations)
-
-const evaluations = [
-  getPositiveEvaluation({
-    label : 'positive',
-    fn    : x => typeof x,
-  }),
-  getNegativeEvaluation({
-    label : 'negative',
-    fn    : x => typeof x,
-  }),
+  { fail : { a : true } },
+  { fail : { b : true } },
+  { danger : null },
 ]
 
-const withGetPositiveEvaluation = {
-  testSuite : 'foo',
-  data      : [ { singleCase } ],
-  evaluations,
+const runTestsInput = {
+  label : 'foo',
+  data  : testData,
+  fn    : testFn,
 }
+runTests(runTestsInput)
 
-runTests(withGetPositiveEvaluation)
+// test('undefined throws', () => {
+//   expect(() => runTests()).toThrow()
+// })
+
+// test('missing `testSuite`', () => {
+//   expect(() =>
+//     runTests(omit('testSuite', runTestsInput))
+//   ).toThrow()
+// })
+
+// runTests(runTestsInput)
+
+// const singleCaseAsync = {
+//   foo : 1,
+//   t   : 'RAMBDAX_DELAY',
+//   f   : 2,
+// }
+
+// const whenTrueAsync = {
+//   label    : '{{tag}} hey hey what can I do',
+//   whenTrue : async x => {
+//     const result = await delay(x.foo)
+
+//     expect(result).toBe(x.t)
+//   },
+// }
+
+// const runTestsInputAsync = {
+//   testSuite   : 'foo',
+//   data        : [ { singleCaseAsync } ],
+//   evaluations : [ whenTrueAsync ],
+// }
+
+// runTests(runTestsInputAsync)
+
+// const withGetEvaluations = {
+//   testSuite   : 'foo',
+//   data        : [ { singleCase } ],
+//   evaluations : getEvaluations({
+//     label : 'BAR',
+//     fn    : x => typeof x,
+//   }),
+// }
+
+// runTests(withGetEvaluations)
+
+// const evaluations = [
+//   getPositiveEvaluation({
+//     label : 'positive',
+//     fn    : x => x > 0,
+//   }),
+//   getNegativeEvaluation({
+//     label : 'negative',
+//     fn    : x => typeof x === 'boolean',
+//   }),
+// ]
+
+// const simpleCase = { foo : 1 }
+
+// const withGetPositiveEvaluation = {
+//   testSuite : 'foo',
+//   data      : [ { simpleCase } ],
+//   evaluations,
+// }
+
+// runTests(withGetPositiveEvaluation)
