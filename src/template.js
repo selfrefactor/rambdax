e24985e75c56f4f0bf1520b65820bfbfa9483055
Brandon Pittman <github@brandonpittman.net>
2020-01-25 10:13:17 +0900

    Add arbitrary expression parsing in curly braces …
    I found myself wanting to push the limits of what `template` can do, but I didn't want to pull in a larger templating library, so I figured we could just allow the template function to handle any expression inside it.

--- src/template.js
+++ src/template.js
@@ -1,31 +1,31 @@
-const getOccurances = input => input.match(/{{[_a-zA-Z0-9]+}}/g)
+const escape = s => s.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&')
 
-const getOccuranceProp = occurance =>
-  occurance.replace(/{{|}}/g, '')
+const getOccurances = input => input.match(/{{\s*.+?\s*}}/g)
 
-const replace = ({ inputHolder, prop, replacer }) =>
-  inputHolder.replace(`{{${ prop }}}`, replacer)
+const getOccuranceProp = occurance => occurance.replace(/{{\s*|\s*}}/g, '')
 
-export function template(input, templateInput){
-  if (arguments.length === 1){
-    return templateInputHolder => template(
-      input,
-      templateInputHolder
-    )
+const replace = ({
+  inputHolder,
+  prop,
+  replacer
+}) => inputHolder.replace(new RegExp(`{{\\s*${escape(prop)}\\s*}}`), replacer)
+
+function template(input, templateInput) {
+  if (arguments.length === 1) {
+    return templateInputHolder => template(input, templateInputHolder)
   }
+
   const occurances = getOccurances(input)
   if (occurances === null) return input
-
   let inputHolder = input
-  for (const occurance of occurances){
+  for (const occurance of occurances) {
     const prop = getOccuranceProp(occurance)
-    const replacer = templateInput[ prop ]
+    const replacer = new Function('templateInput', `with(templateInput) { return ${prop} }`)(templateInput)
 
-    if (replacer === undefined) continue
     inputHolder = replace({
       inputHolder,
       prop,
-      replacer,
+      replacer
     })
   }
 
