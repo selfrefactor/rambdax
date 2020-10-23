0fe384faa6352d826b837922eb9de9ceedf73688
Dejan Toteff <deyan8284@gmail.com>
2020-10-23 16:23:52 +0300

    chore: upgrade rollup setup

--- rollup.web.config.js
+++ rollup.web.config.js
@@ -1,31 +1,31 @@
-const babel = require("rollup-plugin-babel")
-const replace = require("rollup-plugin-replace")
-const resolve = require("rollup-plugin-node-resolve")
-const cleanup = require("rollup-plugin-cleanup")
+import babel from '@rollup/plugin-babel';
+import cleanup from 'rollup-plugin-cleanup'
+import replace from '@rollup/plugin-replace'
+import {nodeResolve}  from '@rollup/plugin-node-resolve'
 
-const extensions = [".js"]
+const extensions = [ '.js' ]
 
 export default {
-  plugins: [
-    replace({
-      'process.env.NODE_ENV': JSON.stringify('production'),
-    }),
-    resolve({
+  plugins : [
+    replace({ 'process.env.NODE_ENV' : JSON.stringify('production') }),
+    nodeResolve({
       extensions,
-      preferBuiltins: false
+      browser: true,
+      preferBuiltins : true,
     }),
     cleanup(),
-    babel({
-      extensions,
-      exclude: ["node_modules/**"]
-		})
+    babel({ 
+      babelHelpers: 'bundled',
+        extensions,
+        exclude : [ 'node_modules/**' ],
+    })
   ],
-  input: "rambdax.js",
-   output : [
+  input  : 'rambdax.js',
+  output : [
     {
       file   : './dist/rambdax.umd.js',
       format : 'umd',
       name   : 'R',
     },
-  ],
-}  
+  ]
+}
