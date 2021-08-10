202e543f5c8398b28a6ef349b10ff70521288515
Dejan Toteff <deyan8284@gmail.com>
2021-08-10 13:41:24 +0300

    fix: wrong ts typings (#73)
    Co-authored-by: Dejan Totev <dejan.totev@psinteractive.io>

--- index.d.ts
+++ index.d.ts
@@ -1421,8 +1421,6 @@ export function when<T, U>(predicate: (x: T) => boolean): ((whenTrueFn: (a: T) =
 
 /**
  * It returns `true` if all each property in `conditions` returns `true` when applied to corresponding property in `input` object.
- * 
- * `R.equals` is used to determine equality.
  */
 export function where<T, U>(conditions: T, input: U): boolean;
 export function where<T>(conditions: T): <U>(input: U) => boolean;
@@ -1978,8 +1976,8 @@ export function lensEq<T>(lens: Lens, target: T): (input: T[]) => boolean;
  */
 export function lensSatisfies<T, U>(predicate: (x: T) => boolean, lens: Lens, input: U): boolean;
 export function lensSatisfies<T, U>(predicate: (x: T) => boolean, lens: Lens): (input: U) => boolean;
-export function lensSatisfies<T>(predicate: (x: T) => boolean, lens: Lens, input: rray<T>): boolean;
-export function lensSatisfies<T>(predicate: (x: T) => boolean, lens: Lens): (input: rray<T>) => boolean;
+export function lensSatisfies<T>(predicate: (x: T) => boolean, lens: Lens, input: T[]): boolean;
+export function lensSatisfies<T>(predicate: (x: T) => boolean, lens: Lens): (input: T[]) => boolean;
 
 /**
  * A combination between `R.defaultTo` and `R.view.
